1a5657592359734ca40cd39e29d4d4cd1cbf74ab
Radulescu Serban-Petre <serban-petre@mail.com>
2021-02-08 13:07:36 +0100

    Import 2
    This is not final but close.

--- Greenhouse/importdata.R
+++ Greenhouse/importdata.R
@@ -1,2 +1,3 @@
-read.csv("\\final.measures.csv",  header = TRUE)
-read.csv("\\measures.days12345.csv",  header = TRUE)
+data.final <- read.csv("final.measures.csv",  header = TRUE)
+data1 <- read.csv("measures.days12345.csv",  header = TRUE)
+
