c3cd24f9d3745243a1185e9365c704b92c0be9b8
Radulescu Serban-Petre <serban-petre@mail.com>
2021-02-08 13:52:33 +0100

    Import data modify 1

--- Greenhouse/importdata.R
+++ Greenhouse/importdata.R
@@ -1,3 +1,4 @@
-data.final <- read.csv("final.measures.csv",  header = TRUE)
-data1 <- read.csv("measures.days12345.csv",  header = TRUE)
+data.final <- read.csv("Greenhouse GMMS - Final measures.csv",  header = TRUE)
+data1 <- read.csv("Greenhouse GMMS - Measures.days12345.csv",  header = TRUE)
+
 
