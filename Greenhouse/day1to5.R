8a7d0b883060f0e80f9c7f548cdc35dcd2569511
Radulescu Serban-Petre <serban-petre@mail.com>
2021-02-08 15:37:27 +0100

    Update for today

--- Greenhouse/day1to5.R
+++ Greenhouse/day1to5.R
@@ -1,1 +1,12 @@
 source("importdata.R")
+
+
+tab <- as.data.frame("species","treatment","average length", "average fresh weight", "average")
+for(i in unique(factor(data.final$species))) { 
+for(j in 1:4) { 
+a1 <- data.final[ which(data.final$species ==i & data.final$treatment == j),]
+
+tab <-as.data.frame(tab,c(i,j,mean(a1$dry.weight),mean(a1$final.length),mean(a1$fresh.weight)))
+
+
+}}
