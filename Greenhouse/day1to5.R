1f0ce6ceaceafdbb816a08744443324d9413ae4a
Radulescu Serban-Petre <serban-petre@mail.com>
2021-02-10 17:41:02 +0100

    Commit from today

--- Greenhouse/day1to5.R
+++ Greenhouse/day1to5.R
@@ -5,12 +5,12 @@ library("ggpubr")
 library(car)
 library(agricolae)
 data.final
-TA2 <- aggregate(list(data.final$dry.weight, data.final$final.length, data.final$fresh.weight,data.final$root.shoot), 
+TA2 <- aggregate(list(data.final$dry.weight, data.final$final.length, data.final$fresh.weight,data.final$root.shoot, data.final$nr.leaves, data.final$pr.growth), 
                  by = list(data.final$treatment, data.final$species),
                  FUN = function(x) c(mean = mean(x))
                  )
 TA2 <- as.data.frame(as.matrix(TA2))
-names(TA2) <- c("treatment", "species", "dry.weight","final.length","fresh.weight","root.shoot")
+names(TA2) <- c("treatment", "species", "dry.weight","final.length","fresh.weight","root.shoot","nr.leaves","pr.growth")
 
 barplot(height=as.numeric(TA2$dry.weight), names=TA2$treatment)
 
@@ -76,18 +76,46 @@ summary(res.aov)
 pairwise.t.test(dt$final.length, dt$treatment ,
                 p.adjust.method = "BH")
 
-##Root shoot
+##4.Root shoot
 res.aov <- aov(lm(root.shoot ~ treatment, dt))
 aov_residuals <- residuals(object = res.aov )
 
 
 leveneTest(root.shoot ~ as.character(treatment), data = dt)
 #From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
-#P Value 0.1166
+#P Value 0.41
 shapiro.test(x = aov_residuals )
-#p-value = 0.03 > 0.05 we can NOT assume normality
-summary(res.aov)
+#p-value = 0.1232 > 0.05 we can  assume normality
+summary(res.aov) #NO SIGNIFICANT Differences
 
+###4.number of leaves
+
+dt <- subset(data.final, data.final$species == "triticum")
+## 1. Dry weight
+
+
+res.aov <- aov(lm(nr.leaves ~ treatment, dt))
+aov_residuals <- residuals(object = res.aov )
+
+
+leveneTest(nr.leaves ~ as.character(treatment), data = dt)
+#From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
+#P Value 0.62
+shapiro.test(x = aov_residuals )
+#p-value = 0.082 > 0.05 we canassume normality
+summary(res.aov) 
+
+pairwise.t.test(dt$nr.leaves, dt$treatment ,
+                p.adjust.method = "BH")
+
+kruskal.test(dry.weight ~ treatment, data = dt)
+#With a P Value smaller than 0.05
+#As the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
+
+
+anova <- lm(dry.weight ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Triticum - dry weight", ylab = "Dry weight", xlab = "Type of spray", sub = "two groups of effects: a and b")
 
 
 ###Lollium
@@ -139,6 +167,43 @@ summary(res.aov)
 
 
 
+##4.Root shoot
+res.aov <- aov(lm(root.shoot ~ treatment, dt))
+aov_residuals <- residuals(object = res.aov )
+
+
+
+
+leveneTest(root.shoot ~ as.character(treatment), data = dt)
+#From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
+#P Value 0.17
+shapiro.test(x = aov_residuals )
+#p-value = 0.7 > 0.05 we can  assume normality
+summary(res.aov) #Significant difference
+
+pairwise.t.test(dt$root.shoot, dt$treatment ,
+                p.adjust.method = "BH")
+#a a a b
+
+
+##nr.leaves
+dt <- subset(data.final, data.final$species == "lolium")
+
+res.aov <- aov(lm(nr.leaves ~ treatment, dt))
+aov_residuals <- residuals(object = res.aov )
+
+
+
+
+leveneTest(nr.leaves ~ as.character(treatment), data = dt)
+#From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
+#P Value 0.74
+shapiro.test(x = aov_residuals )
+#p-value = 0.6333 > 0.05 we can  assume normality
+summary(res.aov) #Significant difference
+
+pairwise.t.test(dt$root.shoot, dt$treatment ,
+                p.adjust.method = "BH")
 
 #Final Length
 
@@ -186,6 +251,24 @@ my_bar <- barplot(as.numeric(TA2$fresh.weight) , border=F , names.arg=TA2$specie
                   ylab = "Weight (g)",
                   ylim=c(0,6) , 
                   main="Fresh weight" )
+#abline(v=c(4.5) , col="grey")
+#text(my_bar + 60 , paste(" a") ,cex=1 )
+par(mar=c(5.1, 4.1, 4.1, 11), xpd=TRUE)
+
+legend(x="topright", legend = c("Control","Water stress","200mM", "500mM" ) , 
+       col = c("green" , "red" ,"blue4" ,  "blue2") , 
+       xpd=TRUE,inset=c(-1.4,-0.2),
+       bty = "n", pch=20 , pt.cex = 2, cex = 1, horiz = FALSE)
+
+##nr.leaves
+
+my_bar <- barplot(as.numeric(TA2$nr.leaves) , border=F , names.arg=TA2$species , 
+                  las=1, space=c(0.25,0.25,0.25,0.25,2.5,0.25,0.25,0.25),
+                  col=c("green" , "red" ,"blue4" ,  "blue2"), 
+                  xlab = "Species", 
+                  ylab = "Weight (g)",
+                  ylim=c(0,12) , 
+                  main="leaves (number)" )
 #abline(v=c(4.5) , col="grey")
 #text(my_bar + 60 , paste(" a") ,cex=1 )
 par(mar=c(5.1, 4.1, 4.1, 11), xpd=TRUE)
