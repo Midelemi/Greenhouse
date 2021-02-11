e95ba6625070a98ddfba0e15a18931a51773a825
Radulescu Serban-Petre <serban-petre@mail.com>
2021-02-11 17:17:07 +0100

    Update

--- Greenhouse/day1to5.R
+++ Greenhouse/day1to5.R
@@ -5,12 +5,12 @@ library("ggpubr")
 library(car)
 library(agricolae)
 data.final
-TA2 <- aggregate(list(data.final$dry.weight, data.final$final.length, data.final$fresh.weight,data.final$root.shoot, data.final$nr.leaves, data.final$pr.growth), 
+TA2 <- aggregate(list(data.final$dry.weight, data.final$final.length, data.final$fresh.weight,data.final$root.shoot, data.final$nr.leaves, data.final$pr.growth, data.final$aerial.length, data.final$root.length), 
                  by = list(data.final$treatment, data.final$species),
                  FUN = function(x) c(mean = mean(x))
                  )
 TA2 <- as.data.frame(as.matrix(TA2))
-names(TA2) <- c("treatment", "species", "dry.weight","final.length","fresh.weight","root.shoot","nr.leaves","pr.growth")
+names(TA2) <- c("treatment", "species", "dry.weight","final.length","fresh.weight","root.shoot","nr.leaves","pr.growth", "aerial.length","root.length")
 
 barplot(height=as.numeric(TA2$dry.weight), names=TA2$treatment)
 
@@ -77,8 +77,11 @@ pairwise.t.test(dt$final.length, dt$treatment ,
                 p.adjust.method = "BH")
 
 ##4.Root shoot
+dt <- subset(data.final, data.final$species == "triticum")
 res.aov <- aov(lm(root.shoot ~ treatment, dt))
 aov_residuals <- residuals(object = res.aov )
+
+
 
 
 leveneTest(root.shoot ~ as.character(treatment), data = dt)
@@ -87,11 +90,23 @@ leveneTest(root.shoot ~ as.character(treatment), data = dt)
 shapiro.test(x = aov_residuals )
 #p-value = 0.1232 > 0.05 we can  assume normality
 summary(res.aov) #NO SIGNIFICANT Differences
+kruskal.test(root.shoot ~ treatment, data = dt)
+#With a P Value smaller than 0.05
+#As the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
+
+
+anova <- lm(root.shoot ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Triticum - root shoot", ylab = "%", xlab = "Type of spray", sub = "two groups of effects: a and b")
+
+
+
+
 
 ###4.number of leaves
 
 dt <- subset(data.final, data.final$species == "triticum")
-## 1. Dry weight
+
 
 
 res.aov <- aov(lm(nr.leaves ~ treatment, dt))
@@ -117,6 +132,55 @@ anova <- lm(dry.weight ~ treatment, data = dt)
 agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
 plot(agri, main = "Triticum - dry weight", ylab = "Dry weight", xlab = "Type of spray", sub = "two groups of effects: a and b")
 
+##root length
+
+dt <- subset(data.final, data.final$species == "triticum")
+
+
+res.aov <- aov(lm(root.length ~ treatment, dt))
+aov_residuals <- residuals(object = res.aov )
+
+
+leveneTest(root.length ~ as.character(treatment), data = dt)
+#From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
+#P Value 0.1166
+shapiro.test(x = aov_residuals )
+#p-value = 0.18 > 0.05 we can NOT assume normality
+summary(res.aov) #Is useless here we can not assume normality
+
+kruskal.test(root.length ~ treatment, data = dt)
+#With a P Value smaller than 0.05
+#As the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
+
+anova <- lm(root.length ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Triticum - root length", ylab = "Dry weight", xlab = "Type of spray")
+
+##aerial.length
+
+dt <- subset(data.final, data.final$species == "triticum")
+
+
+res.aov <- aov(lm(aerial.length ~ treatment, dt))
+aov_residuals <- residuals(object = res.aov )
+
+
+leveneTest(aerial.length ~ as.character(treatment), data = dt)
+#From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
+#P Value 0.032
+
+
+kruskal.test(aerial.length ~ treatment, data = dt)
+#With a P Value smaller than 0.05
+#As the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
+
+anova <- lm(aerial.length ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Triticum - aerial.length", ylab = "Dry weight", xlab = "Type of spray")
+
+
+
+
 
 ###Lollium
 dt <- subset(data.final, data.final$species == "lolium")
@@ -140,6 +204,7 @@ plot(agri, main = "Lollium - dry weight", ylab = "Dry weight", xlab = "Type of s
 
 ##2.fresh.weight
 
+dt <- subset(data.final, data.final$species == "lolium")
 res.aov <- aov(lm(fresh.weight ~ treatment, dt))
 aov_residuals <- residuals(object = res.aov )
 
@@ -151,6 +216,11 @@ shapiro.test(x = aov_residuals )
 #p-value = 0.2455 > 0.05 we can NOT assume normality
 summary(res.aov) 
 #With a P value=0.6 > 0.05 we prove there there are NOT significant difference
+
+anova <- lm(fresh.weight ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Lollium - dry weight", ylab = "Dry weight", xlab = "Type of spray", sub = "two groups of effects: a and b")
+
 
 ##3.final.length
 res.aov <- aov(lm(final.length ~ treatment, dt))
@@ -168,6 +238,7 @@ summary(res.aov)
 
 
 ##4.Root shoot
+dt <- subset(data.final, data.final$species == "lolium")
 res.aov <- aov(lm(root.shoot ~ treatment, dt))
 aov_residuals <- residuals(object = res.aov )
 
@@ -202,8 +273,65 @@ shapiro.test(x = aov_residuals )
 #p-value = 0.6333 > 0.05 we can  assume normality
 summary(res.aov) #Significant difference
 
-pairwise.t.test(dt$root.shoot, dt$treatment ,
+pairwise.t.test(dt$nr.leaves, dt$treatment ,
                 p.adjust.method = "BH")
+
+kruskal.test(nr.leaves ~ treatment, data = dt)
+#With a P Value smaller than 0.05
+#As the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
+
+anova <- lm(nr.leaves ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Triticum - root length", ylab = "Dry weight", xlab = "Type of spray")
+
+
+##root length
+
+dt <- subset(data.final, data.final$species == "lolium")
+
+
+res.aov <- aov(lm(root.length ~ treatment, dt))
+aov_residuals <- residuals(object = res.aov )
+
+
+leveneTest(root.length ~ as.character(treatment), data = dt)
+#From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
+#P Value 0.1166
+shapiro.test(x = aov_residuals )
+#p-value = 0.18 > 0.05 we can NOT assume normality
+summary(res.aov) #Is useless here we can not assume normality
+
+kruskal.test(root.length ~ treatment, data = dt)
+#With a P Value smaller than 0.05
+#As the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
+
+anova <- lm(root.length ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Triticum - root length", ylab = "Dry weight", xlab = "Type of spray")
+
+##aerial.length
+
+dt <- subset(data.final, data.final$species == "lolium")
+
+
+res.aov <- aov(lm(aerial.length ~ treatment, dt))
+aov_residuals <- residuals(object = res.aov )
+
+
+leveneTest(aerial.length ~ as.character(treatment), data = dt)
+#From the output above we can see that the p-value is not less than the significance level of 0.05. This means that there is no evidence to suggest that the variance across groups is statistically significantly different. Therefore, we can assume the homogeneity of variances in the different treatment groups.
+#P Value 0.032
+
+
+kruskal.test(aerial.length ~ treatment, data = dt)
+#With a P Value smaller than 0.05
+#As the p-value is less than the significance level 0.05, we can conclude that there are significant differences between the treatment groups.
+
+anova <- lm(aerial.length ~ treatment, data = dt)
+agri <- HSD.test(anova, 'treatment', alpha = 0.05, group=TRUE, main = "HSD Test")
+plot(agri, main = "Triticum - aerial.length", ylab = "Dry weight", xlab = "Type of spray")
+
+
 
 #Final Length
 
@@ -266,7 +394,7 @@ my_bar <- barplot(as.numeric(TA2$nr.leaves) , border=F , names.arg=TA2$species ,
                   las=1, space=c(0.25,0.25,0.25,0.25,2.5,0.25,0.25,0.25),
                   col=c("green" , "red" ,"blue4" ,  "blue2"), 
                   xlab = "Species", 
-                  ylab = "Weight (g)",
+                  ylab = "number of leaves",
                   ylim=c(0,12) , 
                   main="leaves (number)" )
 #abline(v=c(4.5) , col="grey")
